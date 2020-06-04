64530341f8ca89b91398946ae73ac93d191ea042
Mohizuki <upalphb4@gmail.com>
2020-06-04 16:40:17 +0700

    first commit

---
+++ assets/js/images.js
@@ -0,0 +1,17 @@
+jQuery(function($){
+				
+				$.supersized({
+				
+					// Functionality
+					slide_interval          :   3000,		// Length between transitions
+					transition              :   1, 			// 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
+					transition_speed		:	2000,		// Speed of transition
+															   
+					// Components							
+					slide_links				:	'blank',	// Individual links for each slide (Options: false, 'num', 'name', 'blank')
+					slides 					:  	[			// Slideshow Images
+														{image : 'images/background/01.png'}
+												]
+					
+				});
+		    });
